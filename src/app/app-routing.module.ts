f293c7cd01aa08352960f2c712a317992d6e287b
Ubuntu <saurabh.singh@vnnogile.in>
2022-09-24 11:04:50 +0530

    MAJOR COMMIT --BASE FRAMEWORK USING ANGULAR UNIVERSAL

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
 import { InputComponent } from './shared/components/input/input.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
   {
     path: 'home',
     component: InputComponent
